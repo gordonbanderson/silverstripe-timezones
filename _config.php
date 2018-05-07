31a3be968348af2586b3abdc21ad2c9f31999e3f
Gordon Anderson <gordon.b.anderson@gmail.com>
2018-05-07 11:30:15 +0700

    Initial checkin

---
+++ _config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * SilverStripe Timezones configuration file.
+ *
+ * PHP version >=5.6.0
+ *
+ * For full copyright and license information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ *
+ * @package Suilven\TimeZones
+ * @author Gordon Anderson <gordon.b.anderson@gmail.com>
+ * @copyright 2018 Suilven
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @link https://github.com/gordonbanderson/silverstripe-timezones
+ */
---
+++ admin/client/src/bundles/bundle.js
@@ -0,0 +1,6 @@
+/* SilverStripe Timezones Admin Bundle
+===================================================================================================================== */
+
+// Load Styles:
+
+require('styles/bundle.scss');
